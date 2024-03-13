9add9873c1bb40b5f8d68341f51c1df849a5d467
Andrew Jordaan <andrew.jordaan@platform45.com>
2024-03-13 09:03:07 +0000

    refactor: remove unused code

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,4 @@ Rails.application.routes.draw do
     get '/ready_for_pickup' => 'orders#ready_for_pickup', as: :ready_for_pickup, on: :collection
     post '/mark_as_paid' => 'orders#mark_as_paid', as: :mark_as_paid, on: :member
   end
-  resources :menu_items
 end
